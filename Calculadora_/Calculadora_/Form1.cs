adaba5e56c6f8191af48d4aa0435b94482bd2289
vmsalvato <viniciusmoreirasalvato@gmail.com>
2020-12-25 22:05:39 -0300

    commit up1

--- Calculadora_/Calculadora_/Form1.cs
+++ Calculadora_/Calculadora_/Form1.cs
@@ -19,43 +19,94 @@ namespace Calculadora_
 
         private void btnSoma_Click(object sender, EventArgs e)
         {
-            double num1, num2, resultado;
-            num1 = Convert.ToDouble(txtEscolha.Text);
-            num2 = Convert.ToDouble(txtEscolha2.Text);
-            resultado = num1 + num2;
-            txtResult.Text = resultado.ToString();
-            MessageBox.Show($"O resultado do cálculo é: {resultado}", "RESULTADO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            if (txtEscolha.Text == "")
+            {
+                MessageBox.Show("Digite todos os valores", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
 
+            else if (txtEscolha2.Text == "") 
+            {
+                MessageBox.Show("Digite todos os valores", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            else 
+            {
+                double num1, num2, resultado;
+                num1 = Convert.ToDouble(txtEscolha.Text);
+                num2 = Convert.ToDouble(txtEscolha2.Text);
+                resultado = num1 + num2;
+                txtResult.Text = resultado.ToString();
+                MessageBox.Show($"Resultado do cálculo: {resultado}", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
 
         private void btnSub_Click(object sender, EventArgs e)
         {
-            double num1, num2, resultado;
-            num1 = Convert.ToDouble(txtEscolha.Text);
-            num2 = Convert.ToDouble(txtEscolha2.Text);
-            resultado = num1 - num2;
-            txtResult.Text = resultado.ToString();
-            MessageBox.Show($"O resultado do cálculo é: {resultado}", "RESULTADO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            if (txtEscolha.Text == "")
+            {
+                MessageBox.Show("Digite todos os valores", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            else if (txtEscolha2.Text == "")
+            {
+                MessageBox.Show("Digite todos os valores", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            else
+            {
+                double num1, num2, resultado;
+                num1 = Convert.ToDouble(txtEscolha.Text);
+                num2 = Convert.ToDouble(txtEscolha2.Text);
+                resultado = num1 - num2;
+                txtResult.Text = resultado.ToString();
+                MessageBox.Show($"Resultado do cálculo: {resultado}", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
 
         private void btnMult_Click(object sender, EventArgs e)
         {
-            double num1, num2, resultado;
-            num1 = Convert.ToDouble(txtEscolha.Text);
-            num2 = Convert.ToDouble(txtEscolha2.Text);
-            resultado = num1 * num2;
-            txtResult.Text = resultado.ToString();
-            MessageBox.Show($"O resultado do cálculo é: {resultado}", "RESULTADO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            if (txtEscolha.Text == "")
+            {
+                MessageBox.Show("Digite todos os valores", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            else if (txtEscolha2.Text == "")
+            {
+                MessageBox.Show("Digite todos os valores", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            else
+            {
+                double num1, num2, resultado;
+                num1 = Convert.ToDouble(txtEscolha.Text);
+                num2 = Convert.ToDouble(txtEscolha2.Text);
+                resultado = num1 * num2;
+                txtResult.Text = resultado.ToString();
+                MessageBox.Show($"Resultado do cálculo: {resultado}", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
 
         private void btnDiv_Click(object sender, EventArgs e)
         {
-            double num1, num2, resultado;
-            num1 = Convert.ToDouble(txtEscolha.Text);
-            num2 = Convert.ToDouble(txtEscolha2.Text);
-            resultado = num1 / num2;
-            txtResult.Text = resultado.ToString();
-            MessageBox.Show($"O resultado do cálculo é: {resultado}", "RESULTADO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            if (txtEscolha.Text == "")
+            {
+                MessageBox.Show("Digite todos os valores", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            else if (txtEscolha2.Text == "")
+            {
+                MessageBox.Show("Digite todos os valores", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            else
+            {
+                double num1, num2, resultado;
+                num1 = Convert.ToDouble(txtEscolha.Text);
+                num2 = Convert.ToDouble(txtEscolha2.Text);
+                resultado = num1 / num2;
+                txtResult.Text = resultado.ToString();
+                MessageBox.Show($"Resultado do cálculo: {resultado}", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
 
         private void btnLimp_Click(object sender, EventArgs e)
